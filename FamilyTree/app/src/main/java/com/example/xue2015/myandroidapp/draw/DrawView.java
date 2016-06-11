0b5b47e37fb5fe9d925ce269cb73096dec0a60e6
xueyifei <787706202@qq.com>
2016-06-11 19:26:52 +0800

    修改userName与identity的文字的位置

--- FamilyTree/app/src/main/java/com/example/xue2015/myandroidapp/draw/DrawView.java
+++ FamilyTree/app/src/main/java/com/example/xue2015/myandroidapp/draw/DrawView.java
@@ -22,61 +22,43 @@ public class DrawView extends View{
 
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
-        /*Paint paint = new Paint();
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setColor(Color.GRAY);
-        paint.setStrokeWidth(4);*/
 
         float left = 50;
         float top = 140;
-        float right = left + 200;
-        float bottom = top + 200;
-//        canvas.drawRect(left,top,right,bottom,paint);
-//        canvas.drawRect(left,bottom,right,bottom+80,paint);
-//
-//        Paint paintText = new Paint();
-//        paintText.setTextSize(25);
-//        canvas.drawText("Father",(left + right)*3/8,bottom+30,paintText);
-//        canvas.drawText("XueYifei",(left + right)*3/8,bottom+65,paintText);
-//
-//        Bitmap bitmap =  BitmapFactory.decodeResource(getResources(), R.drawable.usericon);
-//        Rect src = new Rect();
-//        Rect dst = new Rect();
-//
-//        src.top = 10;
-//        src.left = 10;
-//        src.right = 210;
-//        src.bottom = 210;
-//
-//        dst.top = (int) top+5;
-//        dst.left = (int) left+5;
-//        dst.right = (int) right;
-//        dst.bottom = (int) bottom;
-//        canvas.drawBitmap(bitmap,src,dst,null);
 
-        drawNodeFrame(canvas,left,top,right,bottom);
-        drawUserImage(canvas,left,top,right,bottom);
-        drawUserText(canvas,left,top,right,bottom);
+        drawFTNode(canvas,left,top);
+        drawFTNode(canvas,400,top);
+        drawFTNode(canvas,50,600);
+        drawFTNode(canvas,500,600);
 
     }
 
-    public void drawNodeFrame(Canvas canvas, float left, float top,float right,float bottom){
+    public void drawFTNode(Canvas canvas, float left, float top){
+        drawNodeFrame(canvas,left,top);
+        drawUserImage(canvas,left,top);
+        drawUserText(canvas,left,top);
+    }
+
+    public void drawNodeFrame(Canvas canvas, float left, float top){
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setColor(Color.GRAY);
         paint.setStrokeWidth(4);
+
+        float right = left + 200;
+        float bottom = top + 200;
         //add  frame
+
         canvas.drawRect(left,top,right,bottom,paint);
         canvas.drawRect(left,bottom,right,bottom+80,paint);
 
-//        drawUserImage(canvas,left,top,right,bottom);
-
-
     }
 
-    public void drawUserImage(Canvas canvas, float left, float top,float right,float bottom){
+    public void drawUserImage(Canvas canvas, float left, float top){
+        float right = left + 200;
+        float bottom = top + 200;
+
         // add image
         Bitmap bitmap =  BitmapFactory.decodeResource(getResources(), R.drawable.usericon);
         Rect src = new Rect();
@@ -92,12 +74,19 @@ public class DrawView extends View{
         canvas.drawBitmap(bitmap,src,dst,null);
     }
 
-    public  void drawUserText(Canvas canvas, float left, float top,float right,float bottom){
+    public  void drawUserText(Canvas canvas, float left, float top){
+        float right = left + 200;
+        float bottom = top + 200;
+
         //add Text
         Paint paintText = new Paint();
         paintText.setTextSize(20);
-        canvas.drawText("Father",(left + right)*3/8,bottom+30,paintText);
-        canvas.drawText("XueYifei",(left + right)*3/8,bottom+65,paintText);
+        String name = "XueYifei";
+        String identity = "Father";
+
+        canvas.drawText(identity,(left + right)/2 - paintText.measureText(identity)/2 ,bottom+30,paintText);
+        canvas.drawText(name,(left + right)/2 - paintText.measureText(name)/2,bottom+65,paintText);
     }
+
 
 }
