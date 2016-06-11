a496edcebf99ecaf64faca8ceff68887a6d2cd83
xueyifei <787706202@qq.com>
2016-06-11 21:21:29 +0800

    修改TF主页

--- FamilyTree/app/src/main/java/com/example/xue2015/myandroidapp/FamilyTreeActivity.java
+++ FamilyTree/app/src/main/java/com/example/xue2015/myandroidapp/FamilyTreeActivity.java
@@ -3,6 +3,7 @@ package com.example.xue2015.myandroidapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 public class FamilyTreeActivity extends AppCompatActivity {
 
@@ -11,5 +12,12 @@ public class FamilyTreeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_family_tree);
+
+        Titlebar tb = (Titlebar) findViewById(R.id.familyTreeTitle);
+        TextView tv = (TextView) tb.findViewById(R.id.textAxtionBarTitle);
+        tv.setText("家谱树");
+        tb.hideLeft();
+        tb.hideRight();
+
     }
 }
