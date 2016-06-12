58cc7bd3c45123b758b3fbc692027571dff726d9
GIT-SXQ <957164854@qq.com>
2016-06-12 11:10:11 +0800

    增加了Node

---
+++ FamilyTree/app/src/main/java/com/example/xue2015/myandroidapp/draw/FamilyNode.java
@@ -0,0 +1,43 @@
+package com.example.xue2015.myandroidapp.draw;
+
+/**
+ * Created by MyPC on 2016/6/12.
+ */
+public class FamilyNode {
+    private int id;
+    private FamilyMember member;
+    private int father;
+    private int mother;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public FamilyMember getMember() {
+        return member;
+    }
+
+    public void setMember(FamilyMember member) {
+        this.member = member;
+    }
+
+    public int getFather() {
+        return father;
+    }
+
+    public void setFather(int father) {
+        this.father = father;
+    }
+
+    public int getMother() {
+        return mother;
+    }
+
+    public void setMother(int mother) {
+        this.mother = mother;
+    }
+}
