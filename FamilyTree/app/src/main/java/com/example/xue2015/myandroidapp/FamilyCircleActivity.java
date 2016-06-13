3badcbbdc07398606c436c2bad24b1e28c8d2204
xueyifei <787706202@qq.com>
2016-06-13 16:13:48 +0800

    添加了“家族圈”底部导航

--- FamilyTree/app/src/main/java/com/example/xue2015/myandroidapp/FamilyCircleActivity.java
+++ FamilyTree/app/src/main/java/com/example/xue2015/myandroidapp/FamilyCircleActivity.java
@@ -1,9 +1,12 @@
 package com.example.xue2015.myandroidapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class FamilyCircleActivity extends Activity {
@@ -19,5 +22,34 @@ public class FamilyCircleActivity extends Activity {
         tv.setText("家族圈");
 //        tb.hideLeft();
         tb.hideRight();
+
+        RelativeLayout familyCircleButtom_familyTree = (RelativeLayout) findViewById(R.id.familyCircleButtom_familyTree);
+        RelativeLayout familyCircleButtom_connection = (RelativeLayout) findViewById(R.id.familyCircleButtom_connection);
+        RelativeLayout familyCircleButtom_familyMe = (RelativeLayout) findViewById(R.id.familyCircleButtom_familyMe);
+
+        familyCircleButtom_familyTree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it  = new Intent(FamilyCircleActivity.this,FamilyTreeActivity.class);
+                startActivity(it);
+            }
+        });
+
+        familyCircleButtom_connection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it  = new Intent(FamilyCircleActivity.this,ConnectActivity.class);
+                startActivity(it);
+            }
+        });
+
+        familyCircleButtom_familyMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it  = new Intent(FamilyCircleActivity.this,I.class);
+                startActivity(it);
+            }
+        });
+
     }
 }
